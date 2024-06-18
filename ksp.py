2187dacc6ccd23cb4f464a3d4aa9a98f2f4307c0
Cheng-Yu Pai <mail4charliepai@gmail.com>
2024-06-18 02:15:08 -0700

    Add eclipse time function

--- ksp.py
+++ ksp.py
@@ -26,6 +26,9 @@ def orbital_period(a, g):
     g: standard gravitation parameter in m3/s2
     """
     t = 2*math.pi*(a*a*a/g)**0.5
+    return hmst(t)
+
+def hmst(t):
     h = int(t/3600)
     m = int(60*(t/3600-h))
     s = int(60*(60*(t/3600-h)-m))
@@ -33,13 +36,19 @@ def orbital_period(a, g):
 
 def seconds(h, m, s):
     return s + 60*m + 3600*h
-    
+
 def desired_altitude(t: int, body: Body):
     """
     Returns the altitude of an orbit for a desired orbital period t
     """
     a = ((body.G*t*t)/(4*math.pi*math.pi))**(1/3)
     return a - body.r
+
+def eclipse_time(body: Body, altitude: float):
+    a = altitude + body.r
+    _,_,_,P = orbital_period(a, body.G)
+    t = P*math.asin(body.r/a)/math.pi
+    return hmst(t)
 
 def three_commsats(body: Body, antenna_range: int):
     min_orbital_period = orbital_period(body.r*2, body.G)
