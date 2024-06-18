c2cffc4b9361bfccbd4fbedc50946dd73cda86ad
Cheng-Yu Pai <mail4charliepai@gmail.com>
2024-06-17 23:11:55 -0700

    Add printouts for 3 and 4-constellations

--- ksp.py
+++ ksp.py
@@ -1,4 +1,6 @@
-pi = 3.14159265
+import math
+
+# See https://wiki.kerbalspaceprogram.com/wiki/Tutorial:Ideal_Orbits_for_Communication_Satellites
 
 class Body:
     def __init__(self, r, G):
@@ -12,12 +14,46 @@ bodies = {
 }
 
 def orbital_period(a, g):
-    t = 2*pi*(a*a*a/g)**0.5
+    """
+    t: orbital period in seconds
+    a: semi-major axis of the orbit in meters (typically twice the body's radius)
+    g: standard gravitation parameter in m3/s2
+    """
+    t = 2*math.pi*(a*a*a/g)**0.5
     h = int(t/3600)
     m = int(60*(t/3600-h))
     s = int(60*(60*(t/3600-1)-m))
     return (h, m, s, t)
 
-def print_prd(a, g):
-    h, m, s, t = orbital_period(a, g)
-    print(f"{h}h {m}m {s}s")
+def seconds(h, m, s):
+    return s + 60*m + 3600*h
+    
+def desired_altitude(t: int, body: Body):
+    """
+    Returns the altitude of an orbit for a desired orbital period t
+    """
+    a = ((body.G*t*t)/(4*math.pi*math.pi))**(1/3)
+    return a - body.r
+
+def three_commsats(body: Body, antenna_range: int):
+    min_orbital_period = orbital_period(body.r*2, body.G)
+    h,m,s,t = min_orbital_period
+    print(f"Minimum Altitude: {body.r*2-body.r}")
+    print(f"Minimum Orbital Period: {h}h {m}m {s}s")
+    max_a = antenna_range/math.sqrt(3)
+    max_orbital_period = orbital_period(max_a, body.G)
+    h,m,s,t = max_orbital_period
+    print(f"Maximum Altitude: {max_a - body.r}")
+    print(f"Maximum Orbital Period: {h}h {m}m {s}s")
+
+def four_commsats(body: Body, antenna_range: int):
+    min_a = body.r*math.sqrt(2)
+    min_orbital_period = orbital_period(min_a, body.G)
+    h,m,s,t = min_orbital_period
+    print(f"Minimum Altitude: {min_a - body.r}")
+    print(f"Minimum Orbital Period: {h}h {m}m {s}s")
+    max_a = antenna_range/math.sqrt(2)
+    max_orbital_period = orbital_period(max_a, body.G)
+    h,m,s,t = max_orbital_period
+    print(f"Maximum Altitude: {max_a - body.r}")
+    print(f"Maximum Orbital Period: {h}h {m}m {s}s")
